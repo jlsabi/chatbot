a5852acada43bf8a78e27c94e132a9ec71aa8eda
Abiuthan Kuruthas <123639416+jlsabi@users.noreply.github.com>
2023-01-31 11:15:07 +0000

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -7,15 +7,16 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 
 import tensorflow
+import gtts
 
 from tensorflow.keras.models import load_model
 
-lemmatizer =WordNetLemmatizer()
+lemmatizer = WordNetLemmatizer()
 intents = json.loads(open('intents.json').read())
 
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
-model = load_model('chatbot_model.model')
+model = load_model('chatbotmodel.h5')
 
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
@@ -41,21 +42,21 @@ def predict_class(sentence):
     return_list = []
     for r in results:
         return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
-        return return_list
+    return return_list
 
-    def get_response(intents_list, intents_json):
-        tag = intents_list[0]['intent']
-        list_of_intents = intents_json['intents']
-        for i in list_of_intents:
-            if i ['tag'] == tag:
-                result = random.choice(i['responses'])
-                break
-        return result
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
 
-    print(" VITHUKAL IS RUNNING !")
+print(" VITHUKAL IS RUNNING !")
 
-    while True:
-        message = raw_input()
-        ints = predict_class(message)
-        res = get_response(ints, intents)
-    print(res)
+while True:
+    message = input()
+    ints = predict_class(message)
+    res = get_response(ints, intents)
+    print("res")
